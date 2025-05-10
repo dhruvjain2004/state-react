ae0f0f12b1d5f843cba5e96204b7fc27e34054f3
dhruv jain <dhruvjain527@gmail.com>
2025-05-10 15:13:18 +0530

    first commit

---
+++ react-state-class/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './Counter'
+import LikeButton from './LikeButton'
+
+function App() {
+  return (
+    <>
+      <h1>LIKE HERE</h1>
+      <LikeButton/>
+    </>
+  )
+}
+
+export default App
---
+++ react-state-class/src/LikeButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+export default function LikeButton() {
+  let [isliked, setisliked] = useState(false);
+  let togglelike = () => {
+    setisliked(!isliked);
+  };
+
+  let likestyle = { color: "red" };
+  return (
+    <div>
+      <p onClick={togglelike}>
+        {isliked ? (
+          <i className="fa-solid fa-heart" style={likestyle}></i>
+        ) : (
+          <i className="fa-regular fa-heart"></i>
+        )}
+      </p>
+    </div>
+  );
+}
