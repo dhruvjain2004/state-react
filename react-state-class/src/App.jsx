fdeee86e07e9fe9a8b8e0537c33d85739f0c9202
dhruv jain <dhruvjain527@gmail.com>
2025-05-11 10:44:17 +0530

    com

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Counter from './Counter'
 import LikeButton from './LikeButton'
-
+import Form from './Form'
 function App() {
   return (
     <>
-      <h1>LIKE HERE</h1>
+      <Counter/>
+      <br />
+      <Form/>
       <LikeButton/>
     </>
   )
--- react-state-class/src/Counter.jsx
+++ react-state-class/src/Counter.jsx
@@ -4,10 +4,13 @@ export default function Counter() {
     let [count,setCount] = useState(0);
 
     let inCount = () => {
-        setCount(count+1);
-        console.log(count);     
-    };
-
+      setCount((currCount) => {
+        return currCount+1;
+      });
+      setCount((currCount) => {
+        return currCount+1;
+      });
+    }
   return (
     <div>
         <h3>Count={count}</h3>
@@ -15,3 +18,4 @@ export default function Counter() {
     </div>
   );
 }
+
